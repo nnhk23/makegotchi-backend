717c1085bb0fb721684b847ea7b1aec34aa33eed
Kim Nguyen <nnhk23@gmail.com>
2020-12-23 01:38:01 -0800

    alter user controller

--- app/controllers/user_pets_controller.rb
+++ app/controllers/user_pets_controller.rb
@@ -1,2 +1,16 @@
 class UserPetsController < ApplicationController
+
+    def index
+        user = User.find(params[:user_id])
+        user_pets = user.user_pets
+        render json: user_pets.to_json(
+            :include => {
+                :pet => {
+                    :except => [:created_at, :updated_at]
+                }
+            },
+            :except => [:created_at, :updated_at]
+        )
+    end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+
+    def show 
+        user = User.find(params[:id])
+        render json: user.to_json(
+            :include => {
+                :user_pets => {
+                    :except => [:created_at, :updated_at]
+                }
+            },
+            :except => [:created_at, :updated_at]
+        )
+    end
+
+    def update
+        user = User.find(params[:id])
+        user.update(user_params)
+        render json: user
+    end
+
+
+    private
+
+    def user_params
+        params.require(:user).permit(:id, :username, :name, :buys_left)
+    end
 end
