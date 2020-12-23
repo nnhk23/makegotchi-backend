c13c22ccb723e8ccffa3fdccc1e16a5dc905d834
Kim Nguyen <nnhk23@gmail.com>
2020-12-23 00:15:35 -0800

    updated database with valid images

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,8 @@
 class PetsController < ApplicationController
+
+    def index
+        pets = Pet.all
+        render json: pets, except: [:created_at, :updated_at]
+    end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Pet.destroy_all
 
 PET = [
     {
@@ -16,8 +17,8 @@ PET = [
         # 600 seconds (10mins)
         "dirt_rate": 120,
         # 120 seconds (2 mins)
-        "img_icon": "https://static.wikia.nocookie.net/tamagotchi/images/f/f6/Kikitchi_top_hat.png/revision/latest/scale-to-width-down/75?cb=20161001043225",
-        "img_url": "https://static.wikia.nocookie.net/tamagotchi/images/2/23/Kiki_anime.png/revision/latest/scale-to-width-down/662?cb=20150130225113"
+        "img_icon": "https://static.wikia.nocookie.net/tamagotchi/images/c/c2/Kikitchimix.png/revision/latest/scale-to-width-down/44?cb=20160722103818",
+        "img_url": "https://static.wikia.nocookie.net/tamagotchi/images/1/17/Kikitchi_behind.png/revision/latest/scale-to-width-down/340?cb=20110903115321"
     },
     {
         "personality": "Peppy",
@@ -29,19 +30,19 @@ PET = [
         "dirt_rate": 120,
         # 120 seconds (2 mins)
         "img_icon": "https://static.wikia.nocookie.net/tamagotchi/images/d/d0/Mametchi_tah.png/revision/latest/scale-to-width-down/50?cb=20150511023616",
-        "img_url": "https://static.wikia.nocookie.net/tamagotchi/images/c/c2/Little_mametchi.png/revision/latest/scale-to-width-down/109?cb=20201213191434"
+        "img_url": "https://static.wikia.nocookie.net/tamagotchi/images/3/37/03_Mametchi.svg/revision/latest/scale-to-width-down/203?cb=20111125003832"
     },
     {
-        "personality": "Lazy",
-        "species": "Watawatatchi",
+        "personality": "Peppy",
+        "species": "Lovelitchi",
         "hunger_rate": 30, 
         # 30 seconds
         "sleepy_rate": 600,
         # 600 seconds (10mins)
         "dirt_rate": 120,
         # 120 seconds (2 mins)
-        "img_icon": "https://static.wikia.nocookie.net/tamagotchi/images/1/1a/Watawatatchi.png/revision/latest/scale-to-width-down/124?cb=20150212010731",
-        "img_url": "https://static.wikia.nocookie.net/tamagotchi/images/d/d0/Watawatatchi.PNG/revision/latest/scale-to-width-down/259?cb=20150118162415"
+        "img_icon": "https://static.wikia.nocookie.net/tamagotchi/images/4/41/Lovelin_Blue.png/revision/latest/scale-to-width-down/172?cb=20101026082218",
+        "img_url": "https://static.wikia.nocookie.net/tamagotchi/images/8/8d/Lovelitchi_anime.PNG/revision/latest/scale-to-width-down/212?cb=20170729032254"
     },
     {
         "personality": "Peppy",
@@ -52,7 +53,7 @@ PET = [
         # 600 seconds (10mins)
         "dirt_rate": 120,
         # 120 seconds (2 mins)
-        "img_icon": "https://static.wikia.nocookie.net/tamagotchi/images/1/1b/Nyatchi_tah.png/revision/latest/scale-to-width-down/50?cb=20150522025952",
+        "img_icon": "https://static.wikia.nocookie.net/tamagotchi/images/6/67/Nyatchi_original.gif/revision/latest/scale-to-width-down/54?cb=20140218023750",
         "img_url": "https://static.wikia.nocookie.net/tamagotchi/images/c/c4/Nyatchi.png/revision/latest/scale-to-width-down/330?cb=20110917023305"
     },
     {
@@ -65,31 +66,43 @@ PET = [
         "dirt_rate": 120,
         # 120 seconds (2 mins)
         "img_icon": "https://static.wikia.nocookie.net/tamagotchi/images/a/ab/Kuchipatchi_sprite_app1.png/revision/latest?cb=20140311014725",
-        "img_url": "https://static.wikia.nocookie.net/tamagotchi/images/f/fc/Kuchipatchi_tah.png/revision/latest/scale-to-width-down/50?cb=20150514033356"
+        "img_url": "https://static.wikia.nocookie.net/tamagotchi/images/5/53/Kuchipatchi.PNG/revision/latest/scale-to-width-down/350?cb=20110917230213"
     },
     {
-        "personality": "Lazy",
-        "species": "Gozarutchi",
+        "personality": "Cranky",
+        "species": "KuroMametchi",
         "hunger_rate": 30, 
         # 30 seconds
         "sleepy_rate": 600,
         # 600 seconds (10mins)
         "dirt_rate": 120,
         # 120 seconds (2 mins)
-        "img_icon": "https://static.wikia.nocookie.net/tamagotchi/images/e/e7/Gozarutchi_happy.png/revision/latest/scale-to-width-down/174?cb=20150731062143",
-        "img_url": "https://static.wikia.nocookie.net/tamagotchi/images/d/dd/Gozarutchi_anime.png/revision/latest/scale-to-width-down/350?cb=20111010001018"
+        "img_icon": "https://static.wikia.nocookie.net/tamagotchi/images/2/24/Kuromametchim%21x.png/revision/latest/scale-to-width-down/43?cb=20161129211437",
+        "img_url": "https://static.wikia.nocookie.net/tamagotchi/images/6/68/Kuromametchi_blue.PNG/revision/latest/scale-to-width-down/213?cb=20110921100918"
     },
     {
-        "personality": "Lazy",
-        "species": "Hashizoutchi",
+        "personality": "Peppy",
+        "species": "Melodytchi",
         "hunger_rate": 30, 
         # 30 seconds
         "sleepy_rate": 600,
         # 600 seconds (10mins)
         "dirt_rate": 120,
         # 120 seconds (2 mins)
-        "img_icon": "https://static.wikia.nocookie.net/tamagotchi/images/0/0b/Hashizotchi_tah.png/revision/latest/scale-to-width-down/50?cb=20150521033021",
-        "img_url": "https://static.wikia.nocookie.net/tamagotchi/images/a/a5/Hashizoutchi_Large.png/revision/latest/scale-to-width-down/350?cb=20200919164338"
+        "img_icon": "https://static.wikia.nocookie.net/tamagotchi/images/e/e9/ID_Melodytchi.PNG/revision/latest/scale-to-width-down/23?cb=20110831071614",
+        "img_url": "https://static.wikia.nocookie.net/tamagotchi/images/4/41/Melodytchi_anime_pose.png/revision/latest/scale-to-width-down/199?cb=20150913212228"
+    },
+    {
+        "personality": "Lazy",
+        "species": "Mimitchi",
+        "hunger_rate": 30, 
+        # 30 seconds
+        "sleepy_rate": 600,
+        # 600 seconds (10mins)
+        "dirt_rate": 120,
+        # 120 seconds (2 mins)
+        "img_icon": "https://static.wikia.nocookie.net/tamagotchi/images/5/54/Mimitchi_reading.gif/revision/latest/top-crop/width/220/height/220?cb=20140914170311",
+        "img_url": "https://static.wikia.nocookie.net/tamagotchi/images/9/98/Mimitchi_anime.PNG/revision/latest?cb=20150118162441"
     },
     {
         "personality": "Peppy",
@@ -100,19 +113,7 @@ PET = [
         # 600 seconds (10mins)
         "dirt_rate": 120,
         # 120 seconds (2 mins)
-        "img_icon": "https://static.wikia.nocookie.net/tamagotchi/images/b/bd/Ginjirotchi_tah.png/revision/latest/scale-to-width-down/50?cb=20150512230957",
-        "img_url": "https://static.wikia.nocookie.net/tamagotchi/images/9/91/Ginjirotchi_blue.png/revision/latest/scale-to-width-down/350?cb=20111021091258"
-    },
-    {
-        "personality": "Peppy",
-        "species": "Ginjirotchi",
-        "hunger_rate": 30, 
-        # 30 seconds
-        "sleepy_rate": 600,
-        # 600 seconds (10mins)
-        "dirt_rate": 120,
-        # 120 seconds (2 mins)
-        "img_icon": "https://static.wikia.nocookie.net/tamagotchi/images/b/bd/Ginjirotchi_tah.png/revision/latest/scale-to-width-down/50?cb=20150512230957",
+        "img_icon": "https://static.wikia.nocookie.net/tamagotchi/images/6/68/Ginjirotchi_sprite_app1.png/revision/latest/scale-to-width-down/32?cb=20140311013000",
         "img_url": "https://static.wikia.nocookie.net/tamagotchi/images/9/91/Ginjirotchi_blue.png/revision/latest/scale-to-width-down/350?cb=20111021091258"
     }
 ]
