c475b8e7592bccd7005d16aa294b109deb4d60b7
gamil91 <alexis_gamil99@yahoo.com.com>
2020-12-23 09:24:24 -0800

    create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+
+    def create
+        user = User.find_or_create_by(user_params)
+        render json: user, except:[:created_at, :updated_at]
+        # if user.valid?
+        #     render json: user, except:[:created_at, :updated_at]
+        # else 
+        #     error = user.errors.full_messages.first
+        #     render plaintext: error
+        # end
+
+    end
+
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :username)
+    end
+
 end
--- db/migrate/20201222194223_create_users.rb
+++ db/migrate/20201222194223_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       t.string :username
       t.string :name
-      t.integer :buys_left
+      t.integer :buys_left, default: 3
 
       t.timestamps
     end
