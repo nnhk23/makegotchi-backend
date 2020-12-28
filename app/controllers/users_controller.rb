784b6966f8867c6de299e06abf3db946de918ba8
Kim Nguyen <nnhk23@gmail.com>
2020-12-27 19:29:21 -0800

    added buys_left in user serializer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
         user = User.new(user_params)
         if user.save
             token = encode_token(user_id: user.id)
-            render json: {user: user, token: token}
+            render json: {user: UserSerializer.new(user), token: token}
         else
             render json: user.errors
         end
